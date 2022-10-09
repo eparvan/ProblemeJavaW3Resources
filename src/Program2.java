23a06e7cbef463a733aaa31bc6d88f8cf71ebd3e
eparvan <eparvan@gmail.com>
2022-10-09 10:26:15 +0300

    Formatare 1 - 3

--- src/Program2.java
+++ src/Program2.java
@@ -6,21 +6,21 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Program2 {
 
     /*
-    * Write a Java program to sum values of an array
-    * */
+     * Write a Java program to sum values of an array
+     * */
     public static void main(String[] args) {
 
-        Scanner myInput = new Scanner( System.in );
+        Scanner myInput = new Scanner(System.in);
         System.out.print("n=");
         int n = myInput.nextInt();
-        int[] numArray= new int[n];
-        int sum=0;
-        for (int i=0; i<n; i++){
-            numArray[i] = ThreadLocalRandom.current().nextInt(1,100);
-            sum+=numArray[i];
+        int[] numArray = new int[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            numArray[i] = ThreadLocalRandom.current().nextInt(1, 100);
+            sum += numArray[i];
         }
-        System.out.println("Tabelul initial:\n"+ Arrays.toString(numArray));
+        System.out.println("Tabelul initial:\n" + Arrays.toString(numArray));
 
-        System.out.println("\nSuma elementelor:"+sum);
+        System.out.println("\nSuma elementelor:" + sum);
     }
 }
--- src/Program3.java
+++ src/Program3.java
@@ -1,10 +1,14 @@
 public class Program3 {
 
+    /**
+     * 3. Write a Java program to print the following grid
+     */
+
     public static void main(String[] args) {
-        char simbol='-';
-        for (int i=0; i<10; i++){
-            for (int j=0; j<10; j++){
-                System.out.print(simbol+" ");
+        char simbol = '-';
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(simbol + " ");
             }
             System.out.println();
         }
