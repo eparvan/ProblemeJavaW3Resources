3ba30bf506403415695e2bd78821939d19ea5141
eparvan <eparvan@gmail.com>
2022-10-09 10:21:45 +0300

    Adaugare Problema 3

--- src/Program3.java
+++ src/Program3.java
@@ -1,7 +1,12 @@
 public class Program3 {
 
     public static void main(String[] args) {
-
+        char simbol='-';
+        for (int i=0; i<10; i++){
+            for (int j=0; j<10; j++){
+                System.out.print(simbol+" ");
+            }
+            System.out.println();
+        }
     }
-
 }
